957faf54272b0708b635a1ced0a0da1954d4809a
Hendro <hendrowunga073@gmail.com>
2024-10-26 03:34:39 +0700

    add api forget password

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Auth\LoginController as AdminLoginController;
 use App\Http\Controllers\Admin\Auth\LogoutController as AdminLogoutController;
+use App\Http\Controllers\User\Auth\ForgetPasswordController;
 use App\Http\Controllers\User\Auth\LoginController as UserLoginController;
 use App\Http\Controllers\User\Auth\LogoutController as UserLogoutController;
 use App\Http\Controllers\User\Auth\RegisterController as UserRegisterController;
@@ -27,8 +28,8 @@ Route::prefix('v1')->group(function () {
     Route::prefix('user')->group(function () {
         Route::post('/register', [UserRegisterController::class, 'register']);
         Route::post('/login', [UserLoginController::class, 'login']);
-        Route::post('/logout', [UserLogoutController::class, 'logout'])
-            ->middleware('auth:sanctum');
+        Route::post('/logout', [UserLogoutController::class, 'logout'])->middleware('auth:sanctum');
+        Route::post('/password/email', [ForgetPasswordController::class, 'sendResetLinkEmail']);
     });
     // Admin Routes
     Route::prefix('admin')->group(function () {
