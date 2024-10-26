2f9e8af393eb152ba35eafd3cb446a05c3cc61a7
Hendro <hendrowunga073@gmail.com>
2024-10-26 09:54:21 +0700

    create Mail/ResetPasswordMail

--- app/Mail/ResetPasswordMail.php
+++ app/Mail/ResetPasswordMail.php
@@ -31,30 +31,30 @@ class ResetPasswordMail extends Mailable
     /**
      * Get the message envelope.
      */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Reset Password Mail',
-        );
-    }
+    // public function envelope(): Envelope
+    // {
+    //     return new Envelope(
+    //         subject: 'Reset Password Mail',
+    //     );
+    // }
 
     /**
      * Get the message content definition.
      */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
+    // public function content(): Content
+    // {
+    //     return new Content(
+    //         view: 'view.name',
+    //     );
+    // }
 
     /**
      * Get the attachments for the message.
      *
      * @return array<int, \Illuminate\Mail\Mailables\Attachment>
      */
-    public function attachments(): array
-    {
-        return [];
-    }
+    // public function attachments(): array
+    // {
+    //     return [];
+    // }
 }
