edbafca15c993ce96856262b594e8745efc34fd1
Hendro <hendrowunga073@gmail.com>
2024-10-27 13:19:12 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Auth\LoginController as AdminLoginController;
 use App\Http\Controllers\Admin\Auth\LogoutController as AdminLogoutController;
-use App\Http\Controllers\User\Auth\ForgetPasswordController;
 use App\Http\Controllers\User\Auth\LoginController as UserLoginController;
 use App\Http\Controllers\User\Auth\LogoutController as UserLogoutController;
 use App\Http\Controllers\User\Auth\RegisterController as UserRegisterController;
-<<<<<<< HEAD
-=======
+
+
 use App\Http\Controllers\User\Auth\ForgotPasswordController;
->>>>>>> 7-feature-forget-password-user
 use App\Http\Controllers\User\Auth\ResetPasswordController;
 
 /*
@@ -33,9 +31,9 @@ Route::prefix('v1')->group(function () {
     Route::prefix('user')->group(function () {
         Route::post('/register', [UserRegisterController::class, 'register']);
         Route::post('/login', [UserLoginController::class, 'login']);
-<<<<<<< HEAD
+
         Route::post('/logout', [UserLogoutController::class, 'logout'])->middleware('auth:sanctum');
-=======
+
         Route::post('/logout', [UserLogoutController::class, 'logout'])
             ->middleware('auth:sanctum');
         // Password Reset Routes
@@ -44,7 +42,6 @@ Route::prefix('v1')->group(function () {
         // Route::post('/password/reset', [ResetPasswordController::class, 'resetPassword']);
         Route::post('/password/reset', [ResetPasswordController::class, 'resetPassword'])->name('api.password.update');
         Route::get('/password/reset', [ResetPasswordController::class, 'showResetForm'])->name('password.reset.form');
->>>>>>> 7-feature-forget-password-user
     });
 
     // Admin Routes
