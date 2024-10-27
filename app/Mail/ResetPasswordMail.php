53742ffe22557d6d7ec55e66a124ce0feee68b09
Hendro <hendrowunga073@gmail.com>
2024-10-27 12:37:46 +0700

    view sederhana tes forgot password

--- app/Mail/ResetPasswordMail.php
+++ app/Mail/ResetPasswordMail.php
@@ -5,56 +5,60 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class ResetPasswordMail extends Mailable
 {
     use Queueable, SerializesModels;
     public $token;
+    public $email;
 
     /**
      * Create a new message instance.
      */
-    public function __construct($token)
+    public function __construct($token, $email)
     {
         $this->token = $token;
+        $this->email = $email;
     }
 
+    // Inside the ResetPasswordMail class
     public function build()
     {
+        $url = url('/password/reset-form?email=' . urlencode($this->email) . '&token=' . urlencode($this->token));
         return $this->view('emails.reset-password')
-            ->subject('Reset Password Notification');
+            ->subject('Reset Password Notification')
+            ->with(['url' => $url]);
     }
+}
 
-    /**
-     * Get the message envelope.
-     */
-    // public function envelope(): Envelope
-    // {
-    //     return new Envelope(
-    //         subject: 'Reset Password Mail',
-    //     );
-    // }
 
-    /**
-     * Get the message content definition.
-     */
-    // public function content(): Content
-    // {
-    //     return new Content(
-    //         view: 'view.name',
-    //     );
-    // }
+/**
+ * Get the message envelope.
+ */
+// public function envelope(): Envelope
+// {
+//     return new Envelope(
+//         subject: 'Reset Password Mail',
+//     );
+// }
 
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
+/**
+ * Get the message content definition.
+ */
+// public function content(): Content
+// {
+//     return new Content(
+//         view: 'view.name',
+//     );
+// }
+
+/**
+ * Get the attachments for the message.
+ *
+ * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+ */
     // public function attachments(): array
     // {
     //     return [];
     // }
-}
