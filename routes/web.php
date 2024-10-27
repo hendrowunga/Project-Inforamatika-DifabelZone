31c17dd5f5ffce28968ceffdf14f84c054ff4442
Hendro <hendrowunga073@gmail.com>
2024-10-27 12:51:08 +0700

    update web

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\Auth\ForgotPasswordController;
 use App\Http\Controllers\User\Auth\ResetPasswordController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 // Home route
 Route::get('/', function () {
     return view('welcome');
