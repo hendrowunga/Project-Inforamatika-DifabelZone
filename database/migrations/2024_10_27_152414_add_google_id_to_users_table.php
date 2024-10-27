3ccd00d42a9a9b6c54932c9dda0d0d40bd950ec2
Hendro <hendrowunga073@gmail.com>
2024-10-28 03:49:21 +0700

    migrate id google for users

--- database/migrations/2024_10_27_152414_add_google_id_to_users_table.php
+++ database/migrations/2024_10_27_152414_add_google_id_to_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('google_id')->unique()->nullable()->after('email');
+            $table->string('avatar')->nullable();
         });
     }
 
