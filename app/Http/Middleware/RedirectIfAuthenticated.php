f3f522ead3b1e261abcd60fb573e06db0020b03f
Hendro <hendrowunga073@gmail.com>
2024-10-29 08:06:11 +0700

    sedeers

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,14 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                // return redirect(RouteServiceProvider::HOME);
+
+                // Admin
+                if ($guard === 'admin') {
+                    return redirect()->route('admin.home');
+                }
+
+                // Seller
             }
         }
 
