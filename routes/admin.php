f656da9d6cbb919444db6ae611164456b97ccbd9
Hendro <hendrowunga073@gmail.com>
2024-11-01 14:21:12 +0700

    update route proile admin

--- routes/admin.php
+++ routes/admin.php
@@ -24,5 +24,6 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
         Route::view('/home', 'back.pages.admin.home')->name('home');
         Route::post('/logout_handler', [AdminController::class, 'logoutHandler'])->name('logout_handler');
+        Route::get('/profile', [AdminController::class, 'profileView'])->name('profile');
     });
 });
