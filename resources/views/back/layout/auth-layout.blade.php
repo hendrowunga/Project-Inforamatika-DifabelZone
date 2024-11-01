c81ca0bd4e9bdd7176b518fec1ff53f1a0f2e461
Hendro <hendrowunga073@gmail.com>
2024-11-01 12:07:44 +0700

    update; view layout page

--- resources/views/back/layout/auth-layout.blade.php
+++ resources/views/back/layout/auth-layout.blade.php
@@ -61,6 +61,15 @@
     <script src="/back/vendors/scripts/script.min.js"></script>
     <script src="/back/vendors/scripts/process.js"></script>
     <script src="/back/vendors/scripts/layout-settings.js"></script>
+
+    <script>
+        if (navigator.userAgent.indexOf("Firefox") != -1) {
+            history.pushState(null, null, document.URL);
+            window.addEventListener('popstate', function() {
+                history.pushState(null, null, document.URL);
+            });
+        }
+    </script>
     @stack('scripts')
 </body>
 
