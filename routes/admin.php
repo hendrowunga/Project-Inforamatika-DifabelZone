ec61833ceada55e1aa8087df6f4df6f63129fc17
Hendro <hendrowunga073@gmail.com>
2024-11-05 21:15:37 +0700

    update; route admin

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\Auth\AdminController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\CategoriesController;
 use App\Models\Admin;
 
 Route::prefix('admin')->name('admin.')->group(function () {
@@ -29,5 +30,20 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::view('/settings', 'back.pages.settings')->name('settings');
         Route::post('/change-logo', [AdminController::class, 'changeLogo'])->name('change-logo');
         Route::post('/change-favicon', [AdminController::class, 'changeFavicon'])->name('change-favicon');
+    });
+
+    //CATEGORIES AND SUB CATEGORIES MANAGEMENT
+    Route::prefix('manage-categories')->name('manage-categories.')->group(function () {
+        Route::controller(CategoriesController::class)->group(function () {
+            Route::get('/', 'catSubcatList')->name('cats-subcats-list');
+            Route::get('/add-category', 'addCategory')->name('add-category');
+            Route::post('/store-category', 'storeCategory')->name('store-category');
+            // Route::get('/edit-category', 'editCategory')->name('edit-category');
+            // Route::post('/update-category', 'updateCategory')->name('update-category');
+            // Route::get('/add-subcategory', 'addSubCategory')->name('add-subcategory');
+            // Route::post('/store-subcategory', 'storeSubCategory')->name('store-subcategory');
+            // Route::get('/edit-subcategory', 'editSubCategory')->name('edit-subcategory');
+            // Route::post('/update-subcategory', 'updateSubCategory')->name('update-subcategory');
+        });
     });
 });
