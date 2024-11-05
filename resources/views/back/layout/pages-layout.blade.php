207e78a0a837394ae22c94aa98f3111b5949a483
Hendro <hendrowunga073@gmail.com>
2024-11-05 17:04:29 +0700

    update; pages layout

--- resources/views/back/layout/pages-layout.blade.php
+++ resources/views/back/layout/pages-layout.blade.php
@@ -375,44 +375,53 @@
                 <div>
                     @yield('content')
                 </div>
-
             </div>
+            <div class="footer-wrap pd-20 mb-20 card-box">
+                DifabZone - Admin By
+                @if (Auth::check())
+                    <strong>{{ Auth::user()->name }}</strong>
+                @else
+                    Not logged in
+                @endif
+            </div>
+        </div>
+    </div>
 
-            <!-- js -->
-            <script src="/back/vendors/scripts/core.js"></script>
-            <script src="/back/vendors/scripts/script.min.js"></script>
-            <script src="/back/vendors/scripts/process.js"></script>
-            <script src="/back/vendors/scripts/layout-settings.js"></script>
-            <script>
-                if (navigator.userAgent.indexOf("Firefox") != -1) {
-                    history.pushState(null, null, document.URL);
-                    window.addEventListener('popstate', function() {
-                        history.pushState(null, null, document.URL);
-                    });
-                }
-            </script>
-            <script src="/extra-assets/ijabo/ijabo.min.js"></script>
-            <script src="/extra-assets/ijabo/jquery.ijaboViewer.min.js"></script>
-            <script src="/extra-assets/ijaboCropTool/ijaboCropTool.min.js"></script>
-            <script>
-                window.addEventListener('showToastr', function(event) {
-                    toastr.remove();
-                    if (event.detail[0].type === 'info') {
-                        toastr.info(event.detail[0].message);
-                    } else if (event.detail[0].type === 'success') {
-                        toastr.success(event.detail[0].message);
-                    } else if (event.detail[0].type === 'error') {
-                        toastr.error(event.detail[0].message);
-                    } else if (event.detail[0].type === 'warning') {
-                        toastr.warning(event.detail[0].message);
-                    } else {
-                        return false;
-                    }
-                });
-            </script>
+    <!-- js -->
+    <script src="/back/vendors/scripts/core.js"></script>
+    <script src="/back/vendors/scripts/script.min.js"></script>
+    <script src="/back/vendors/scripts/process.js"></script>
+    <script src="/back/vendors/scripts/layout-settings.js"></script>
+    <script>
+        if (navigator.userAgent.indexOf("Firefox") != -1) {
+            history.pushState(null, null, document.URL);
+            window.addEventListener('popstate', function() {
+                history.pushState(null, null, document.URL);
+            });
+        }
+    </script>
+    <script src="/extra-assets/ijabo/ijabo.min.js"></script>
+    <script src="/extra-assets/ijabo/jquery.ijaboViewer.min.js"></script>
+    <script src="/extra-assets/ijaboCropTool/ijaboCropTool.min.js"></script>
+    <script>
+        window.addEventListener('showToastr', function(event) {
+            toastr.remove();
+            if (event.detail[0].type === 'info') {
+                toastr.info(event.detail[0].message);
+            } else if (event.detail[0].type === 'success') {
+                toastr.success(event.detail[0].message);
+            } else if (event.detail[0].type === 'error') {
+                toastr.error(event.detail[0].message);
+            } else if (event.detail[0].type === 'warning') {
+                toastr.warning(event.detail[0].message);
+            } else {
+                return false;
+            }
+        });
+    </script>
 
-            @livewireScripts()
-            @stack('scripts')
+    @livewireScripts()
+    @stack('scripts')
 </body>
 
 </html>
