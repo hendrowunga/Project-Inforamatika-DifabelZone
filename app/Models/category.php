1ab4bb6c3500a2e0c6398c827a6b31f1e83c9c1f
Hendro <hendrowunga073@gmail.com>
2024-11-06 14:11:08 +0700

    update; bug category crud from admin

--- app/Models/category.php
+++ app/Models/category.php
@@ -5,23 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class category extends Model
+class Category extends Model
 {
     use HasFactory;
-
-    protected $primaryKey = 'id'; // Menggunakan ID sebagai primary key
-    public $incrementing = true; // ID adalah auto-incrementing
-    protected $keyType = 'int'; // Tipe kunci adalah integer
-
-    protected $fillable = [
-        'name',
-        'deskripsi'
-    ];
-
-    public function products()
-    {
-        return $this->hasMany(Product::class, 'category_id'); // Mengubah user_id sesuai dengan ID
-    }
-
-    
 }
