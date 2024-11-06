5cda38d565551620f2e7cd5158acf837dc70bb1b
Hendro <hendrowunga073@gmail.com>
2024-11-06 13:56:58 +0700

    update; migration database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->string('password');
-            $table->enum('role', ['user', 'admin'])->default('user');
+            // $table->enum('role', ['user', 'admin'])->default('user');
             $table->timestamps();
         });
 
--- database/migrations/2024_10_27_155730_create_products_table.php
+++ database/migrations/2024_10_27_155730_create_products_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Schema::create('category', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->string('name');
-        //     $table->text('deskripsi');
-        //     $table->timestamps();
-        // });
+        Schema::create('category', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('deskripsi');
+            $table->timestamps();
+        });
 
         Schema::create('products', function (Blueprint $table) {
             $table->id();
