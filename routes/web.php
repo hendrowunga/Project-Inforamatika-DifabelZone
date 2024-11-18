784d4d172feadefd56fa6f2e86d77dd813d7b212
putrastepanus <potatocarotz@gmail.com>
2024-11-18 14:12:52 +0700

    add Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\User\Auth\ForgotPasswordController;
-use App\Http\Controllers\User\Auth\ResetPasswordController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\FrontEndController;
-use App\Http\Controllers\Admin\CategoriesController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -19,55 +14,18 @@ use App\Http\Controllers\Admin\CategoriesController;
 |
 */
 
-// Home route
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// // View for requesting a password reset
-// Route::get('/forgot-password', function () {
-//     return view('auth.forgot-password');
-// })->name('password.request');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// // Handle the email submission for password reset
-// Route::post('/password/email', [ForgotPasswordController::class, 'forgotPassword'])->name('password.email');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// // View for displaying the reset password form (with token)
-// // Route::get('/password/reset-form', [ResetPasswordController::class, 'showResetForm'])->name('password.reset.form');
-
-// // Handle the actual password reset process
-// Route::post('/password/reset', [ResetPasswordController::class, 'resetPassword'])->name('password.reset.process');
-
-
-//ADMIN
-// Route::get('/admin', [ProductController::class, 'index']);
-
-// Route::post('/admin/product', [ProductController::class, 'store'])->name('admin.product.store');
-
-// Route::get('/admin/product/{id}/edit', [ProductController::class, 'edit'])->name('admin.product.edit');
-
-// Route::put('/admin/product/{id}', [ProductController::class, 'store'])->name('admin.product.update');
-
-// Route::delete('/admin/product/{id}', [ProductController::class, 'destroy'])->name('admin.product.destroy');
-
-// Route::get('/admin/products/reload', [ProductController::class, 'reloadProducts'])->name('admin.products.reload');
-
-
-
-
-// Admin
-Route::view('/example-page', 'example-page');
-Route::view('/example-auth', 'example-auth');
-Route::view('example-frontend', 'example-frontend');
-
-//login user
-Route::view('/login-user', 'user.login-user')->name('login');
-
-//Dashboatd user
-Route ::view('/dashboard-user', 'user.dashboard-user')->name('dashboard');
-
-//donation user
-route ::View('/donation-user','user.donation-user')->name ('donation');
-
-//about us user
-Route ::view('/about-user','user.about-user')->name ('about');
+require __DIR__.'/auth.php';
