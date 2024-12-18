6a5b5bc33d24573fdb6ca85649c7e2371ea2747d
Hendro <hendrowunga073@gmail.com>
2024-12-18 21:51:46 +0700

    frontend-backend #131

--- routes/web.php
+++ routes/web.php
@@ -44,22 +44,21 @@ Route::view('/about-user', 'user.about-user')->name('about');
 require __DIR__ . '/auth.php';
 
 //register user
-// Route ::view('/register-user','user.register-user')->name ('register');
-Route::get('/register-user', [RegisterController::class, 'showRegisterForm'])->name('register');
-Route::post('/register-user', [RegisterController::class, 'register']);
+// // Route ::view('/register-user','user.register-user')->name ('register');
+// Route::get('/register-user', [RegisterController::class, 'showRegisterForm'])->name('register');
+// Route::post('/register-user', [RegisterController::class, 'register']);
 
-// Halaman login
-Route::get('/login-user', [LoginController::class, 'showLoginForm'])->name('login');
+// // Halaman login
+// Route::get('/login-user', [LoginController::class, 'showLoginForm'])->name('login');
 
-// Proses login
-Route::post('/login-user', [LoginController::class, 'login'])->name('login.submit');
+// // Proses login
+// Route::post('/login-user', [LoginController::class, 'login'])->name('login.submit');
 
 //keranjang
-Route ::view('/cart-user','user.cart-user')->name ('keranjang');
+Route::view('/cart-user', 'user.cart-user')->name('keranjang');
 
 // Menampilkan profil user
 Route::get('/profile/{id}', [ProfileController::class, 'showProfile'])->name('profile.show');
 
 // Menambahkan alamat baru
 Route::post('/profile/{id}/add-address', [ProfileController::class, 'addAddress'])->name('profile.addAddress');
-
